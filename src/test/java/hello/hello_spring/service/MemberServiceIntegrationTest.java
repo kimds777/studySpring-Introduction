7bd52f831830f7068230f75826539feef5ed3b8a
Daseul Kim <tina27849@gmail.com>
2025-02-20 15:10:45 +0900

    Feat : 순수 JDBC

---
+++ src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,67 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void duplicateMemberException() {
+        //given
+        Member member1 = new Member();
+        member1.setName("hello");
+
+        Member member2 = new Member();
+        member2.setName("hello");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("This member already exists.");
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        }catch (IllegalStateException e){
+//            assertThat(e.getMessage()).isEqualTo("This member already exists.");
+//        }
+        //then
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
